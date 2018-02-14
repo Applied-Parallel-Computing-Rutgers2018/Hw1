9f93dd5d15a4acc29ce5fc2934ae3285bf0226d0
John Grun <john.grun@gmail.com>
2018-02-13 21:39:34 -0500

    This version correctly works at about 16 percent. No sse or AVX instructions have been added ..... yet

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -21,7 +21,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 
 void transpose(const double *A, double * B, const int lda);
 void direct_Blocked_Copy(const int lda, const int block, const int N, const int K, const int K_Offset, const int J_Offset, double * B_Block, const double * B);
-void Transposed_Blocked_Copy(const int lda, const int block, const int M, const int K, const int K_Offset, const int i_Offset, double * A_Block, const double * A);
+//void Transposed_Blocked_Copy(const int lda, const int block, const int M, const int K, const int K_Offset, const int i_Offset, double * A_Block, const double * A);
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
@@ -31,63 +31,108 @@ void Transposed_Blocked_Copy(const int lda, const int block, const int M, const 
 static void do_block (int lda, int block, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; ++i)
-    /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      for (int k = 0; k < K; ++k)
+
+  for (int j = 0; j < N; ++j) 
+  {
+
+      for (int i = 0; i < M; i+=4)
       {
-	       //cij += A[i+k*lda] * B[k+j*lda];
-        double Aelement = A[k+i*block];
-        double Belement = B[k+j*block];;
-        cij += Aelement * Belement;
-      }
-      C[i+j*lda] = cij;
+        /* For each column j of B */ 
+          /* Compute C(i,j) */
+          // unneeded memory access. 
+          register double cij = 0;
+          // register double cij0 = 0; 
+          // register double cij1 = 0; 
+          // register double cij2 = 0; 
+          // register double cij3 = 0; 
+
+            for (int k = 0; k < K; ++k)
+            {
+      	       cij += A[i+k*lda] * B[k+j*lda];
+              // cij0 += A[k+i*block] * B[k+j*block];
+              // cij1 += A[k+((1+i)*block)] * B[k+j*block];
+              // cij2 += A[k+((2+i)*block)] * B[k+j*block];
+              // cij3 += A[k+((3+i)*block)] * B[k+j*block];
+
+            }
+
+          C[i+j*lda] += cij;
+          // C[i+1+j*lda] += cij1;
+          // C[i+2+j*lda] += cij2;
+          // C[i+3+j*lda] += cij3;
+
+
+        }
+
     }
-}
+  }
 
 
-inline void transpose(const double *A, double * B, const int lda)
-{
-    int i, j;
-    for (j = 0; j < lda; j++)
-    {
-      int j_lda = j*lda;
-      for (i = 0; i < lda; i++) 
-      {
-          //B[i][j] = A[j][i];
-          B[i + j_lda] = A[j + i * lda];
+// inline void transpose(const double *A, double * B, const int lda)
+// {
+//     int i, j;
+//     for (j = 0; j < lda; j++)
+//     {
+//       int j_lda = j*lda;
+//       for (i = 0; i < lda; i++) 
+//       {
+//           //B[i][j] = A[j][i];
+//           B[i + j_lda] = A[j + i * lda];
 
-      }
-    }
-}
+//       }
+//     }
+// }
 
 
 
 inline void direct_Blocked_Copy(const int lda, const int block, const int N, const int K, const int K_Offset, const int J_Offset, double * B_Block, const double * B)
 {
+
+    register int ColOrginal = 0;
+    register int ColMapped = 0;
+
     for (int J_load = 0; J_load < N; ++J_load)
     {
-      for (int K_load = 0; K_load < K; ++K_load)
+      ColOrginal = (J_load + J_Offset) *lda; /// the col from the orginal matrix
+      ColMapped = J_load * block; // the newly mapped index in the blocked matrix
+
+      for (int K_load = 0; K_load < K; K_load+=4)
       {
-        //B_Block[K_load + J_load*BLOCK_SIZE] = B[k_Block + K_load + J_load*lda];
-        B_Block[K_load + J_load * block] = B[K_load + K_Offset + (J_load + J_Offset) *lda];
-          //B_Block[Index] =  B[k_Block + Index];
+        //j_load = 0
+        B_Block[K_load + ColMapped] = B[K_load + K_Offset + ColOrginal];
+        B_Block[K_load + ColMapped + 1] = B[K_load + K_Offset + ColOrginal + 1];
+        B_Block[K_load + ColMapped + 2] = B[K_load + K_Offset + ColOrginal + 2];
+        B_Block[K_load + ColMapped + 3] = B[K_load + K_Offset + ColOrginal + 3];
+
+        //j_load = 1
+
+
+        //j_load = 2
+
+
+        //j_load = 3
+
+
       } 
     } 
 }
 
 inline void Transposed_Blocked_Copy(const int lda, const int block, const int M, const int K, const int K_Offset, const int i_Offset, double * A_Block, const double * A)
 {
+
+
     for(int I_load = 0; I_load < M; ++I_load)
     {
-      for (int K_load = 0; K_load < K; ++K_load)
+      //int ColOrginal = ((K_load + K_Offset) *lda); /// the col from the orginal matrix
+      //int RowMapped = (I_load * block); // the newly mapped index in the blocked matrix
+      for (int K_load = 0; K_load < K; K_load+=4)
       {
-          //A_Block[K_load + I_load * BLOCK_SIZE] =  A[K_load + k_Block + (I_load + j_Block) *lda];
           A_Block[K_load + (I_load * block)] =  A[i_Offset + I_load + ((K_load + K_Offset) *lda)];
+          A_Block[K_load + (I_load * block) + 1] =  A[i_Offset + I_load + ((K_load +1  + K_Offset) *lda)];
+          A_Block[K_load + (I_load * block) + 2] =  A[i_Offset + I_load + ((K_load + 2 + K_Offset) *lda)];
+          A_Block[K_load + (I_load * block) + 3] =  A[i_Offset + I_load + ((K_load + 3 + K_Offset) *lda)];
       } 
+
     } // for(int I_load = 0; I_load < M; ++I_load)
 }
 
@@ -115,7 +160,7 @@ void square_dgemm (int lda, double* A, double* B, double* C)
         for (int k = 0; k < lda; k += BLOCK_SIZE)
         {
           K = min (BLOCK_SIZE, lda-k);
-          
+
           /* For each block-column of B */
           direct_Blocked_Copy(lda, BLOCK_SIZE,N,K, k, j,B_Block,B);
 
